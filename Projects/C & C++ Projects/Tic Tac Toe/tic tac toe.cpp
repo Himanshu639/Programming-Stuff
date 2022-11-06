be47e996dfc623b93cdc57704813ea51df3f9eb3
Himanshu <himanshurule03@gmail.com>
2022-11-06 20:09:35 +0530

    basic structure of tic tac toe has been created

--- Projects/C & C++ Projects/Tic Tac Toe/tic tac toe.cpp
+++ Projects/C & C++ Projects/Tic Tac Toe/tic tac toe.cpp
@@ -12,54 +12,12 @@ using namespace std;
 
 class TTT {
     char ctrl;
-    int ip = 0, decf,chance = 0;
+    int ip = 0;
     string name;
     char xo1, xo2;
     char grid[3][3];
     public:
-        int check() {
-            if(grid[0][0]=='O' && grid[0][1]=='O' && grid[0][2]=='O') {
-                decf = 69; return 0;
-            }
-            else if(grid[1][0]=='O' && grid[1][1]=='O' && grid[1][2]=='O') {
-                decf = 69;return 0;
-            }
-            else if(grid[2][0]=='O' && grid[2][1]=='O' && grid[2][2]=='O') {
-                decf = 69;return 0;
-            }else if(grid[0][0]=='O' && grid[1][0]=='O' && grid[2][0]=='O') {
-                decf = 69;return 0;
-            }else if(grid[0][1]=='O' && grid[1][1]=='O' && grid[2][1]=='O') {
-                decf = 69;return 0;
-            }else if(grid[0][2]=='O' && grid[1][2]=='O' && grid[2][2]=='O') {
-                decf = 69;return 0;
-            }else if(grid[0][0]=='O' && grid[1][1]=='O' && grid[2][2]=='O') {
-                decf = 69;return 0;
-            }else if(grid[0][2]=='O' && grid[1][1]=='O' && grid[2][0]=='O') {
-                decf = 69;return 0;
-            }
-            if(grid[0][0]=='X' && grid[0][1]=='X' && grid[0][2]=='X') {
-                decf = 420;return 0;
-            }
-            else if(grid[1][0]=='X' && grid[1][1]=='X' && grid[1][2]=='X') {
-                decf = 420;return 0;
-            }
-            else if(grid[2][0]=='X' && grid[2][1]=='X' && grid[2][2]=='X') {
-                decf = 420;return 0;
-            }else if(grid[0][0]=='X' && grid[1][0]=='X' && grid[2][0]=='X') {
-                decf = 420;return 0;
-            }else if(grid[0][1]=='X' && grid[1][1]=='X' && grid[2][1]=='X') {
-                decf = 420;return 0;
-            }else if(grid[0][2]=='X' && grid[1][2]=='X' && grid[2][2]=='X') {
-                decf = 420;return 0;
-            }else if(grid[0][0]=='X' && grid[1][1]=='X' && grid[2][2]=='X') {
-                decf = 420;return 0;
-            }else if(grid[0][2]=='X' && grid[1][1]=='X' && grid[2][0]=='X') {
-                decf = 420;return 0;
-            } else {
-                return 1;
-            }
-            
-        }
+        
         void single_player() {
             char k = 49;
             int score_1 = 0,score_2 = 0;
@@ -84,13 +42,7 @@ class TTT {
             cout<<setw(25)<<right<<"Choose X or O : ";
             cin>>xo1;
             
-            while(1) {
-                if (xo1=='X') {xo2='O';break;}
-                else if (xo1=='O') {xo2='X';break;}
-                else if (xo1=='x') {xo1='X';break;xo2='O';}
-                else if (xo1=='o') {xo1='O';break;xo2='X';}
-                else cout<<endl<<endl<<setw(20)<<right<<"Choose from X or O";
-            }
+            
             
 
             cout<<"\n\n\n";
@@ -108,60 +60,7 @@ class TTT {
                 for(int j=0;j<3;j++)
                     grid[i][j] = ' ';
 
-            while(check()) {
-                system("cls");
-                cout << endl << endl << setw(55) << right << "* * * Tic Tac Toe * * *";
-                cout<<endl<<endl<<setw(15)<<name<<" => "<<score_1<<setw(90)<<right<<"Computer => "<<score_2;
-                
-
-                cout<<endl<<endl<<endl;
-                cout<<setw(45)<<right<<grid[0][0]<<" | "<<grid[0][1]<<" | "<<grid[0][2]<<endl;
-                cout<<setw(54)<<right<<"----------"<<endl;
-                cout<<setw(45)<<right<<grid[1][0]<<" | "<<grid[1][1]<<" | "<<grid[1][2]<<endl;
-                cout<<setw(54)<<right<<"----------"<<endl;
-                cout<<setw(45)<<right<<grid[2][0]<<" | "<<grid[2][1]<<" | "<<grid[2][2]<<endl;
-                
-                int pos;
-                if(chance<9) {
-                    if (chance%2==0) {
-                        cin>>pos;
-                        if (pos>6) {
-                            grid[2][pos-7]=xo1;chance++;
-                        } else if (pos>3) {
-                            grid[1][pos-4]=xo1;chance++;
-                        } else if (pos>0) {
-                            grid[0][pos-1]=xo1;chance++;
-                        }
-                    }
-                    else {
-                        cin>>pos;
-                        if (pos>6) {
-                            grid[2][pos-7]=xo2;chance++;
-                        } else if (pos>3) {
-                            grid[1][pos-4]=xo2;chance++;
-                        } else if (pos>0) {
-                            grid[0][pos-1]=xo2;chance++;
-                        }
-                    }
-                }
-            }
-            if (decf==69) {
-                if (xo1=='O') {
-                    cout<<endl<<endl<<setw(35)<<right<<"Congrautions"<<name<<" is the winner";
-                }
-                else {
-                    cout<<endl<<endl<<setw(35)<<right<<"You lose!!!";
-                }
-                
-            } else {
-                if (xo1=='X') {
-                    cout<<endl<<endl<<setw(35)<<right<<"Congrautions"<<name<<" is the winner";
-                }
-                else {
-                    cout<<endl<<endl<<setw(35)<<right<<"You lose!!!";
-                }
-                
-            }
+            
         }
         void multi_player() {
 
