9e168e2e74a92dc9343df60e30a519dd79168cac
Himanshu <himanshurule03@gmail.com>
2022-11-29 07:26:58 +0530

    inserting and printing added in linked list

--- Practicals/FDS/A5/linkedlist.cpp
+++ Practicals/FDS/A5/linkedlist.cpp
@@ -10,13 +10,31 @@ class node {
 int main() {
     node *head, *n;
     head = new node; n = new node;
+
+    //inserting data for head node
     cout<<"Enter the data for head: ";
     cin>>head->data;
-    n=head->next;
+    head->next=n;
+
+    //inserting data for ordinary node
+    cout<<"Enter the data for ordinary nodes: ";
     for(int i=0;i<4;i++) {
-        cout<<"Enter the data for nodes: ";
         cin>>n->data;
+        n->next = new node;
         n=n->next;
+    }
+
+
+    //printing head node
+    cout<<"\n\nData at head: ";
+    cout<<head->data;
+
+
+    //printing ordinary node
+    for(int i=0;i<4;i++) {
+        head=head->next;
+        cout<<"\nData at ordinary node"<<i+1<<": ";
+        cout<<head->data;
     }
     
 }
