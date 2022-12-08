0cfdc3ae9ae54acb212e4240e5d142703e7f0d9f
Himanshu <himanshurule03@gmail.com>
2022-12-08 22:42:54 +0530

    did some minor changes

--- Practicals/FDS/A1/operation on marks.py
+++ Practicals/FDS/A1/operation on marks.py
@@ -4,18 +4,25 @@ ab=0
 smark=[]
 
 for i in range(n):
-    ipmark=input("Enter marks of Student %d:- " %(i+1))
-    if ipmark=='AB':
-        ab+=1
-        continue
+    while(1):
+        ipmark=input("Enter marks of Student %d:- " %(i+1))
+        if ipmark=='AB':
+            ab+=1
+            ipmark=-1
+            break
+        ipmark = int(ipmark)
+        if (ipmark<=0 or ipmark>=100):
+            print("Please Enter valid marks out of 100")
+        else:
+            break
     smark.append(int(ipmark))
-n-=ab
 
 def ave():
     sum=0
     for i in range(n):
-        sum+=smark[i]
-    print("The average of the all marks is ",(sum/n))
+        if (smark[i]!=-1):
+            sum+=smark[i]
+    print("The average of the all marks is ",(sum/(n-ab)))
 
 def max():
     max=smark[0]
@@ -27,7 +34,7 @@ def max():
 def min():
     min=smark[0]
     for i in range(n):
-        if min>smark[i]:
+        if (min>smark[i] and smark[i]!=-1):
             min=smark[i]
     print("The maximum mark from all the marks is ",min)
 
@@ -35,23 +42,22 @@ def abs():
     print("%d student/s is/are absent" %ab)
 
 def hfreq():
-    occ=[]
+    hfreq = 0
+    index = 0
     for i in range(n):
-        count=0
+        count = 0
+        if (smark[i]==-1):
+            continue
         for j in range(n):
-            if smark[i]==smark[j]:
+            if (smark[i]==smark[j]):
                 count+=1
-        occ.append(count)
-    max=occ[0]
-    index=0
-    for i in range(n):
-        if max<occ[i]:
-            max=occ[i]
-            index=i
-    if max==1:
+        if count > hfreq:
+            hfreq = count
+            index = i
+    if hfreq == 1:
         print("None of the mark has frequency greater than 1\n")
     else:
-        print("Mark with highest frequecy is %d with frequency %d" %(smark[index],max))
+        print("Highest Frequency Mark:- %d\nFreq:- %d" %(smark[index],hfreq))
 
 while(1):
     print("\n\n\n* * * * MENU * * * *\n1. Find Average of marks\n2. Find the highest mark\n3. Find the lowest mark\n4. Find No. of Absent stu.\n5. Find the Highest freq. mark\n")
